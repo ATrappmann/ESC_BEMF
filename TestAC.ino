3bd5ddbcea4a70085379ebfe31e9571c98d805c6
Andreas Trappmann <andreas.trappmann@t-online.de>
2021-04-15 19:03:04 +0200

    Corrections

--- TestAC.ino
+++ TestAC.ino
@@ -3,8 +3,10 @@
 // DESC: Test the Analog Comparator (AC) of an Atmega328p
 //
 
+#include <Arduino.h>
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (!Serial);
   Serial.println(F("TestAC running..."));
   Serial.println("Uploaded: "  __DATE__  " " __TIME__);
@@ -13,11 +15,15 @@ void setup() {
   ADCSRA = (0 << ADEN);     // Disable the ADC module
   ADCSRB = (1 << ACME);     // MUX select for negative input of comparator
   ADMUX = 0;                // Select A0 as comparator negative input
-//  ACSR |= Bx00011011;       // Set IRQ on rising edge, enable AC IRQ and clear ACI
-
+  ACSR |= B00011011;        // Set IRQ on rising edge, enable AC IRQ and clear ACI
 }
 
+unsigned long lastMillis = 0L;
 void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastMillis < 50L) return;
+  lastMillis = currentMillis;
+  
   byte acStatus = ACSR;
   Serial.print(F("ACSR=0b")); Serial.print(acStatus, BIN);
   if (0 != (acStatus & (1<<ACO))) {
@@ -32,24 +38,26 @@ void loop() {
  */
 ISR (ANALOG_COMP_vect) {
   Serial.print("*");
-/*
+
   for (int i=0; i<10; i++) {            // We check the comparator 10 times just to be sure
-    if (ACSR & 0x01) {                  // If step is with IRQ on rising BEMF, ACO should be 0
-      if (0 != (ACSR & (1<<ACO)))) {
-        i--;  // ACO = 1, check again
-        Serial.print("#");
-      }
-    } else {                            // If step is with IRQ on falling BEMF, ACO should be 1
-      if (0 == (ACSR & (1<<ACO)))) {
+    if (ACSR & 0x01) {                  // If step is with IRQ on rising BEMF, ACO should be 1
+      if (0 == (ACSR & (1<<ACO))) {
         i--;  // ACO = 0, check again
-        Serial.print("#");
+        Serial.print("#"); Serial.println(ACSR, BIN);
+      }
+    } else {                            // If step is with IRQ on falling BEMF, ACO should be 0
+      if (0 != (ACSR & (1<<ACO))) {
+        i--;  // ACO = 1, check again
+        Serial.print("+"); Serial.println(ACSR, BIN);
+      }
     }
   }
-*/
+/*
   // Toggle between rising and falling edge
   if (0 != (ACSR & 0x01)) {
     ACSR &= ~0x01;    // Set interrupt on falling edge
   } else {
     ACSR |= 0x01;     // Set interrupt on rising edge
   }
+*/
 }
